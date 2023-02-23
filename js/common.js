56ecce7a7b508484cb9028c73a1ed17085890133
Ziaq <zixrom@gmail.com>
2023-02-23 13:12:30 +0000

    complete project

--- js/common.js
+++ js/common.js
@@ -5,7 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
     loop: true,
     slidesPerView: 3,
     speed: 1800,
-    centeredSlides: true
+    centeredSlides: true,
+    navigation: {
+      nextEl: '.showcase-navigation__next',
+      prevEl: '.showcase-navigation__prev'
+    }
   })
 
   document.querySelector('video').playbackRate = 2; // ускоряем видео в два раза
